c86013cf3e8645d7f0bdb6e934af8ff71b94ddb1
Jerry Huang <jerryhh8@gmail.com>
2025-04-25 08:41:15 -0400

    Create example.py

---
+++ example.py
@@ -0,0 +1,12 @@
+"""
+Name:       Countingthing
+Coded by:   Jerry Huang
+Purpose:    It counts
+Date:       4/25/25
+"""
+
+counter = 0;
+
+while(counter < 10):
+    print("Time: " + str(counter));
+    counter += 1;
